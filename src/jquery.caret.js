74b70ba038c2a699b84c977e4384e09c2e050725
Timur R Mingaliev <timur@mingaliev.com>
2012-07-26 10:54:28 +0400

    Caret position setter & getter

---
+++ src/jquery.caret.js
@@ -0,0 +1,55 @@
+(function($){
+	if( typeof $ == 'undefined'){ return false; }
+	$.fn.caret = function( options ){
+		var defaults = {};
+
+		var methods = {
+			"pos": function(){
+				var args = Array.prototype.slice.call(arguments);
+
+				if( args.length > 0 ){
+					// Set caret position
+					this.focus();
+					if( this.createTextRange ){		// Internet Explorer support
+						var range = this.createTextRange();
+						range.move('character', args[0]);
+						range.select();
+					}else if( this.setSelectionRange ){		// Other browsers
+						this.setSelectionRange( args[0], args[0] );
+					};
+					return $(this);
+				}else{
+					// Get caret position
+					if( document.selection ){	// Internet Explorer support
+						this.focus();
+						var range = document.selection.createRange();
+						range.moveStart('character', -this.value.length);
+						return range.text.length;
+					}else if( this.selectionStart || this.selectionStart == '0' ){
+						return this.selectionStart;
+					};
+					return 0;
+				};
+				return this;
+			}
+		};
+	
+		if( typeof options == 'string' ){
+			var args = Array.prototype.slice.call(arguments);
+			if( methods[options] ){
+				args.splice(0, 1);
+				if( this.length && this.length > 1 ){
+					var result = [];
+					this.each(function(){
+						result.push( methods[options].apply(this, args) );
+					});
+					return result;
+				}
+				return methods[options].apply(this[0], args);
+			};
+			return this;
+		};
+		
+		return this;
+	};
+})(jQuery);
