bb5abf2bdac33fff210aa86f27c4ee55ae045bc5
Timur R Mingaliev <timur@mingaliev.com>
2012-08-13 15:17:51 +0400

    1

--- production/jquery.extra-inputs.js
+++ production/jquery.extra-inputs.js
@@ -1,4 +1,167 @@
 (function($){
+	if( typeof $ == 'undefined'){ return false; }
+	$.fn.caret = function( options ){
+		if (/iPad|iPhone/.test(navigator.platform)) {
+			return this;
+		};
+
+		var defaults = {};
+
+		var methods = {
+			"pos": function(){
+				var args = Array.prototype.slice.call(arguments);
+
+				if( args.length > 0 ){
+					// Set caret position
+					this.focus();
+					if( this.createTextRange ){		// Internet Explorer support
+						var range = this.createTextRange();
+						range.move('character', args[0]);
+						range.select();
+					}else if( this.setSelectionRange ){		// Other browsers
+						this.setSelectionRange( args[0], args[0] );
+					};
+					return $(this);
+				}else{
+					// Get caret position
+					if( document.selection ){	// Internet Explorer support
+						this.focus();
+						var range = document.selection.createRange();
+						range.moveStart('character', -this.value.length);
+						return range.text.length;
+					}else if( this.selectionStart || this.selectionStart == '0' ){ // Other browsers
+						return this.selectionStart;
+					};
+					return 0;
+				};
+				return this;
+			}
+		};
+	
+		if( typeof options == 'string' ){
+			var args = Array.prototype.slice.call(arguments);
+			if( methods[options] ){
+				args.splice(0, 1);
+				if( this.length && this.length > 1 ){ // If selected more than one element, return array with results
+					var result = [];
+					this.each(function(){
+						result.push( methods[options].apply(this, args) );
+					});
+					return result;
+				}
+				return methods[options].apply(this[0], args);
+			};
+			return this;
+		};
+		
+		return this;
+	};
+})(jQuery);/*
+	jQuery.caret plugin from this package required
+*/
+
+(function($){
+	$.fn.group = function( options ){
+		if (/iPad|iPhone/.test(navigator.platform)) {
+			return this;
+		};
+
+		var defaults = {};
+
+		var methods = {
+			"keyup": function(e){
+				var key = e.keyCode,
+					length = $(this).val().length,
+					maxlength = $(this).attr('maxlength') - 0,
+					next = $(this).data('group').next,
+					previous = $(this).data('group').previous,
+					position = $(this).caret('pos');
+
+				if( key == 37 && position == 0 && previous ){ // Left arrow -> Focus to previous input
+					previous.caret('pos', previous.val().length);
+				}else if( key == 39 && position == length && next ){ // Right arrow -> Focus to next input
+					next.caret('pos', 0);
+				}else if( position == maxlength && next ){
+					next.caret('pos', next.val().length);
+				}else if( key == 8 && position == 0 && previous ){
+					previous.caret('pos', previous.val().length);
+				};
+				return true;
+			},
+			"getValue": function(){
+				var value = [], length = [];
+				for( var i = 0, l = inputs.length; i < l; i++ ){
+					var input = $(inputs[i]);
+					value.push(input.val());
+					length.push(input.attr('maxlength') - 0);
+				};
+				return {value: value, len: length};
+			},
+			"setValue": function(value){
+				for( var i = 0, l = inputs.length; i < l; i++ ){
+					var input = $(inputs[i]);
+					input.val( value[i] );
+				}
+				return true;
+			},
+			"paste": function( clipboardData, index, pos ){
+				var data = clipboardData.split(''),
+					value = methods.getValue(),
+					caret = [0, 0];
+
+				for( var i = index, l = value.value.length; i < l; i++ ){
+					var val = value.value[i],
+						s = ( i == index ? pos : 0),
+						ending = val.substr(s);
+					caret = [i, 0];
+					data = data.concat( ending.split('') );
+					value.value[i] = value.value[i].replace( ending, '' );
+					for( var j = value.len[i] - s; j--; ){
+						value.value[i] += data.splice(0, 1).join('');
+						if( data.length > 0)
+							caret[1]++;
+					}
+				}
+
+				methods.setValue(value.value);
+
+				$(inputs[caret[0]]).caret('pos', caret[1] - 1);
+
+				return true;
+			}
+		};
+
+		var inputs = this;
+		for( var i = 0, l = inputs.length; i < l; i++ ){
+			var input = $(inputs[i]);
+			input.data('group', {
+				index: i,
+				name: input.attr('data-group'), 
+				next: ( i+1 == l ? null : $(inputs[i+1])),
+				previous: ( i-1 >= 0 ? $(inputs[i-1]) : null)
+			});
+		};
+
+		this
+			.bind('keyup keypress', methods.keyup)
+			.bind('group-afterpaste', function(e){
+				var tmp = $('#group-clipboard-data');
+				var clipboardData = tmp.val();
+				tmp.remove();
+				methods.paste( clipboardData, $(this).data('group').index, $(this).caret('pos') );
+				this.focus();
+			})
+			.bind('paste', function(e){
+				var tmp = $('<input type="text" id="group-clipboard-data"/>').css({ position: 'absolute', left: '-9999999px' }),
+					self = $(this);
+				$(document.body).append(tmp);
+				tmp[0].focus();
+				setTimeout(function(){ self.trigger('group-afterpaste'); }, 0)
+			});	
+
+		return this;
+	}
+})(jQuery);(function($){
 	$.fn.mask = function( options ){
 		var definitions = {
 			"#": /[0-9]/,
